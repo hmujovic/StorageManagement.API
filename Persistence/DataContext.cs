364437bb2af5ebd91025390bc823a8e6826f2ecd
Haris Mujovic <haris.mujovic@institut.digital>
2024-10-12 11:31:47 +0200

    added initial projects; configured database; created domain models;

---
+++ Persistence/DataContext.cs
@@ -0,0 +1,46 @@
+﻿global using Domain.Entities;
+global using Microsoft.AspNetCore.Identity;
+global using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+global using Microsoft.EntityFrameworkCore;
+global using Microsoft.EntityFrameworkCore.Metadata.Builders;
+global using Persistence.Configurations;
+
+namespace Persistence;
+
+public sealed class DataContext(DbContextOptions options) : IdentityDbContext<
+    Account,
+    AccountRole,
+    string,
+    IdentityUserClaim<string>, // TUserClaim
+    AccountIdentityUserRole, // TUserRole,
+    IdentityUserLogin<string>, // TUserLogin
+    IdentityRoleClaim<string>, // TRoleClaim
+    IdentityUserToken<string> // TUserToken
+>(options)
+{
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        base.OnModelCreating(modelBuilder);
+
+        modelBuilder.Entity<Account>().ToTable("AspNetUsers");
+        modelBuilder.Entity<AccountIdentityUserRole>().ToTable("AspNetUserRoles");
+        modelBuilder.Entity<AccountRole>().ToTable("AspNetRoles");
+        modelBuilder.Entity<AccountIdentityUserRole>()
+            .HasOne(p => p.User)
+            .WithMany(b => b.Roles)
+            .HasForeignKey(p => p.UserId);
+
+        modelBuilder.Entity<AccountIdentityUserRole>()
+            .HasOne(x => x.Role)
+            .WithMany(x => x.Roles)
+            .HasForeignKey(p => p.RoleId);
+
+        modelBuilder.ApplyConfiguration(new RoleConfiguration());
+        modelBuilder.ApplyConfiguration(new AccountConfiguration());
+        modelBuilder.ApplyConfiguration(new UserRoleConfiguration());
+    }
+
+    public DbSet<Product> Products { get; set; }
+    public DbSet<Category> Categories { get; set; }
+    public DbSet<ProductCategory> ProductCategories { get; set; }
+}
--- StorageManagement.API/Program.cs
+++ StorageManagement.API/Program.cs
@@ -1,25 +1,100 @@
+using Domain.Entities;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Persistence;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-
-builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+ConfigureServices(builder.Services);
+ConfigureDatabase(builder.Services, builder.Configuration);
+ConfigureJwtAuthentication(builder.Services, builder.Configuration);
+ConfigureCors(builder.Services);
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
-
-app.UseHttpsRedirection();
-
-app.UseAuthorization();
-
-app.MapControllers();
+ConfigureApp(app, builder.Environment);
 
 app.Run();
+
+void ConfigureServices(IServiceCollection services)
+{
+    services.AddControllers().AddApplicationPart(typeof(Presentation.AssemblyReference).Assembly);
+    services.AddEndpointsApiExplorer();
+    services.AddSwaggerGen();
+
+    //services.AddScoped<IServiceManager, ServiceManager>();
+    //services.AddScoped<IRepositoryManager, RepositoryManager>();
+    //services.AddScoped<ITokenService, TokenService>();
+
+    services.AddIdentity<Account, AccountRole>(opt =>
+    {
+        opt.Password.RequiredLength = 7;
+        opt.Password.RequireDigit = true;
+        opt.Password.RequireUppercase = true;
+        opt.SignIn.RequireConfirmedEmail = true;
+    })
+        .AddEntityFrameworkStores<DataContext>()
+        .AddDefaultTokenProviders();
+}
+
+void ConfigureDatabase(IServiceCollection services, IConfiguration configuration)
+{
+    services.AddDbContextPool<DataContext>(options =>
+    {
+        options.UseMySQL(configuration.GetConnectionString("MainDB"),
+            mysqlOptions => { mysqlOptions.EnableRetryOnFailure(1, TimeSpan.FromSeconds(5), null); });
+    });
+}
+
+void ConfigureJwtAuthentication(IServiceCollection services, IConfiguration configuration)
+{
+    //var jwtSettings = configuration.GetSection("JwtSettings");
+
+    //services.AddAuthentication(opt =>
+    //{
+    //    opt.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+    //    opt.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+    //}).AddJwtBearer(options =>
+    //{
+    //    options.TokenValidationParameters = new TokenValidationParameters
+    //    {
+    //        ValidateIssuer = false,
+    //        ValidateAudience = false,
+    //        ValidateLifetime = true,
+    //        ValidateIssuerSigningKey = true,
+    //        ValidIssuer = jwtSettings["validIssuer"],
+    //        ValidAudience = jwtSettings["validAudience"],
+    //        IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(jwtSettings["securityKey"]))
+    //    };
+    //});
+}
+
+void ConfigureCors(IServiceCollection services)
+{
+    services.AddCors(options =>
+    {
+        options.AddPolicy("Storage", builder =>
+        {
+            builder.AllowAnyOrigin()
+                .AllowAnyMethod()
+                .AllowAnyHeader();
+        });
+    });
+}
+
+void ConfigureApp(WebApplication app, IHostEnvironment environment)
+{
+    app.UseCors("Storage");
+
+    if (environment.IsDevelopment())
+    {
+        app.UseSwagger();
+        app.UseSwaggerUI();
+    }
+
+    app.UseHttpsRedirection();
+    app.UseRouting();
+    app.UseAuthentication();
+    app.UseAuthorization();
+    app.MapControllers();
+}
